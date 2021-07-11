0554454b1c5a4aeeba356e98eb6312e36968112e
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-11 02:05:09 -0300

    Final da aula Nomeando as classes

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,11 +1,12 @@
 import Planet from '../../../assets/images/elements/planet.png';
-let footer = `
-<div>
-    <p>
-    Star wars  -  este é um exemplo criado para os Alunos Gama Academy
-    Módulo de boas práticas.
-    </p>
 
+let footer = `
+<div class="footer-contain">
+    <p>
+        Star wars  -  este é um exemplo criado para os Alunos Gama Academy</br>
+        Módulo de boas práticas.
+    </p>
+    <a href="#section-one">
     <img src=${Planet} alt='planet' width=auto height="180px">
 </div>
 `
